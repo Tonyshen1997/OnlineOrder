9fbd712ceb61aff03037669909b389d41a11108a
Tong Shen <tshen70@gatech.edu>
2022-08-19 03:00:02 -0700

    foodlist added, components intergated

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,63 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Form, Input, message } from "antd";
+import React from "react";
+import { login } from "../utils";
+
+class LoginForm extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  onFinish = (data) => {
+    this.setState({
+      loading: true,
+    });
+
+    login(data)
+      .then(() => {
+        message.success("Login Successful");
+        this.props.onSuccess();
+      })
+      .catch((err) => {
+        message.error(err.message);
+      })
+      .finally(() => {
+        this.setState({
+          loading: false,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <Form
+        onFinish={this.onFinish}
+        style={{
+          width: 300,
+          margin: "auto",
+        }}
+      >
+        <Form.Item
+          name="username"
+          rule={[{ requried: true, message: "Please input your Username!" }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={this.state.loading}>
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
--- src/utils.js
+++ src/utils.js
@@ -1,83 +1,329 @@
-export const login = (credential) => {
-  const loginUrl = `/login?username=${credential.username}&password=${credential.password}`;
+const resData = 
+[
+  {
+      "id": 1,
+      "address": "773 N Mathilda Ave, Sunnyvale, CA 94085",
+      "name": "Burger King",
+      "phone": "(408) 736-0101",
+      "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/store%2Fheader%2F10171.png",
+      "menuItemList": [
+          {
+              "id": 1,
+              "name": "Chicken Fries - 9 Pc",
+              "description": "Made with white meat chicken, our Chicken Fries are coated in a light crispy breading seasoned with savory spices and herbs.",
+              "price": 4.89,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=300,format=auto,quality=50/https://cdn.doordash.com/media/photos/f439436f-c5ab-47af-bac4-7b73ab60a24b-retina-large.jpg"
+          },
+          {
+              "id": 2,
+              "name": "Whopper Meal",
+              "description": "Our Whopper Sandwich is a 1/4 lb* of savory flame-grilled beef topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun.",
+              "price": 10.59,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=300,format=auto,quality=50/https://cdn.doordash.com/media/photos/f878a689-618b-4c70-a00f-e7b1f320adc9-retina-large.jpg"
+          },
+          {
+              "id": 3,
+              "name": "Impossible™ Whopper",
+              "description": "Our Impossible™ Whopper Sandwich features a savory flame-grilled patty made from plants topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun",
+              "price": 7.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/5c306a5f-fdd2-41d2-a660-9762aaa8eee8-retina-large.jpg"
+          },
+          {
+              "id": 4,
+              "name": "HERSHEY'S® Sundae Pie",
+              "description": "Say hello to our HERSHEY’S® Sundae Pie. One part crunchy chocolate crust and one part chocolate crème filling, garnished with a delicious topping and real HERSHEY’S® Chocolate Chips",
+              "price": 3.09,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/80b1670d-e9c0-4886-a5b7-1ad48edd24ca-retina-large.jpg"
+          },
+          {
+              "id": 5,
+              "name": "Whopper",
+              "description": "Our Whopper Sandwich is a 1/4 lb* of savory flame-grilled beef topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun.",
+              "price": 6.39,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/9b3d7985-e457-43b3-938d-5184f48c2687-retina-large-jpeg"
+          },
+          {
+              "id": 6,
+              "name": "Double Whopper Meal",
+              "description": "Our Double Whopper Sandwich is a pairing of two 1/4 lb* savory flame-grilled beef patties topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun",
+              "price": 11.69,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/45addf4a-e8a8-47cb-a705-cce1d10ce86d-retina-large.jpg"
+          },
+          {
+              "id": 7,
+              "name": "Spicy Crispy Chicken Sandwich",
+              "description": "Our Double Whopper Sandwich is a pairing of two 1/4 lb* savory flame-grilled beef patties topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun",
+              "price": 6.09,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/31dd68c2-06ec-42ad-bcd4-da7bd3425437-retina-large-jpeg"
+          },
+          {
+              "id": 8,
+              "name": "Original Chicken Sandwich",
+              "description": "Our Original Chicken Sandwich is lightly breaded and topped with a simple combination of shredded lettuce and creamy mayonnaise on a sesame seed bun",
+              "price": 6.09,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/3e437f54-fa4e-4e9d-bf80-8a1e5b120f32-retina-large-jpeg"
+          },
+          {
+              "id": 9,
+              "name": "Bacon King Sandwich Meal",
+              "description": "Our Bacon King Sandwich features two 1/4 lb* savory flame-grilled beef patties, topped a with hearty portion of thick-cut smoked bacon, melted American cheese and topped with ketchup and creamy mayonnaise all on a soft sesame seed bun.",
+              "price": 12.19,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/adb96c32-3c5b-4375-ba92-b30767d2513d-retina-large.jpg"
+          },
+          {
+              "id": 10,
+              "name": "Classic OREO® Shake",
+              "description": "Cool down with our creamy hand spun OREO® Shake.",
+              "price": 3.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/c3ad483f-bad7-44f1-96af-4c3dcfc63c6d-retina-large.jpg"
+          }
+      ]
+  },
+  {
+      "id": 2,
+      "address": "3450 El Camino Real #105, Santa Clara, CA 95051",
+      "name": "SGD Tofu House",
+      "phone": "(408) 261-3030",
+      "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/store%2Fheader%2F1579.jpg",
+      "menuItemList": [
+          {
+              "id": 11,
+              "name": "Original Soft Tofu",
+              "description": "Tofu boiled with your choice of meat and mushrooms. Served with your choice of side and an assortment of kimchi dishes.",
+              "price": 17.06,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/b7055ca9-3caf-4d9d-9c99-04be1e36dbbf-retina-large-jpeg"
+          },
+          {
+              "id": 12,
+              "name": "Combination Soft Tofu",
+              "description": "Tofu boiled with beef, shrimp, and clams. Served with your choice of side and an assortment of kimchi dishes.",
+              "price": 17.06,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/37ad1974-1395-4e5c-86ff-fdf120cf8c58-retina-large-jpeg"
+          },
+          {
+              "id": 13,
+              "name": "Seafood Soft Tofu",
+              "description": "Tofu boiled with mussels, shrimp, and clam. Served with your choice of side and an assortment of kimchi dishes.",
+              "price": 17.06,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/96bc8289-1950-4b4f-823d-12f33349a5fe-retina-large-jpeg"
+          },
+          {
+              "id": 14,
+              "name": "Seafood Pancake",
+              "description": "Squid, clam, imitation crab, and grilled onions fried in batter.",
+              "price": 20.27,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/0a94b7e9-903d-49b7-937a-7940c8b56ad5-retina-large-jpeg"
+          },
+          {
+              "id": 15,
+              "name": "Kimchi Soft Tofu",
+              "description": "Tofu boiled with kimchi and your choice of meat. Served with your choice of side and an assortment of kimchi dishes.",
+              "price": 17.06,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/0c062cff-1868-40e1-946d-29d3e46f1541-retina-large-jpeg"
+          },
+          {
+              "id": 16,
+              "name": "Beef Short Ribs",
+              "description": "Beef short ribs served with rice and an assortment of kimchi dishes.",
+              "price": 29.36,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/6340c369-2485-4d60-afcf-ca9068448d84-retina-large.jpg"
+          },
+          {
+              "id": 17,
+              "name": "Dumpling Soft Tofu",
+              "description": "Tofu boiled with dumplings, rice cake, and beef. Served with your choice of side and an assortment of kimchi dishes.",
+              "price": 17.06,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/b7055ca9-3caf-4d9d-9c99-04be1e36dbbf-retina-large-jpeg"
+          },
+          {
+              "id": 18,
+              "name": "Assorted Mushroom Tofu",
+              "description": "Tofu boiled with assorted mushrooms. Served with your choice of side and an assortment of kimchidishes",
+              "price": 17.06,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/b7055ca9-3caf-4d9d-9c99-04be1e36dbbf-retina-large-jpeg"
+          },
+          {
+              "id": 19,
+              "name": "BBQ Beef & Vegetables in Stoneware",
+              "description": "Rice, BBQ beef, and vegetables served in stoneware with an assortment of kimchi dishes",
+              "price": 20.27,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/9844dd4e-3c74-4942-8f90-2b3f4be25049-retina-large-jpeg"
+          },
+          {
+              "id": 20,
+              "name": "Ham & Cheese Soft Tofu",
+              "description": "Tofu boiled with ham and cheese. Served with your choice of side and an assortment of kimchi dishes.",
+              "price": 17.06,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/9c6b2a1c-1e2c-4d80-a111-2bebbcadd64c-retina-large.jpg"
+          }
+      ]
+  },
+  {
+      "id": 3,
+      "address": "163 S Murphy Ave, Sunnyvale, CA 94086",
+      "name": "Fashion Wok",
+      "phone": "(408) 739-8866",
+      "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/store%2Fheader%2F273997.jpg",
+      "menuItemList": [
+          {
+              "id": 21,
+              "name": "农家小炒肉Stir Fried Pork with Pepper",
+              "description": "Medium spicy.",
+              "price": 13.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/5b34852e-d253-461c-8be8-1bb0bc5e39be-retina-large.jpg"
+          },
+          {
+              "id": 22,
+              "name": "三味茄子Eggplant with Minced Pork, Garlic, Cilantro",
+              "description": "",
+              "price": 14.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/bf70f262-0c55-41e1-89bc-84c061ae485f-retina-large.jpg"
+          },
+          {
+              "id": 23,
+              "name": "大盆花菜Stir Fried Cauliflower with Pork",
+              "description": "Mild spicy.",
+              "price": 14.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/cb870c77-ace1-49ec-aa2f-9e18de102242-retina-large.jpg"
+          },
+          {
+              "id": 24,
+              "name": "酸汤鱼片Poached Fish Fillets in Sour Soup",
+              "description": "Mild spicy.",
+              "price": 17.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/1acf9c6b-189d-4583-a151-7ef522c283d9-retina-large.jpg"
+          },
+          {
+              "id": 25,
+              "name": "小炒黄牛肉Stir Fried Beef with Pepper",
+              "description": "Very spicy.",
+              "price": 16.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/7f05859d-5e83-476d-a45a-73a3eb8a94e0-retina-large.jpg"
+          },
+          {
+              "id": 26,
+              "name": "武冈香干炒肚丝Stir Fried Shredded Tripe with Wugang Tofu",
+              "description": "Medium spicy.",
+              "price": 19.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/8b2ca9fc-2c1d-4bf2-96ff-d0bd3c415e8d-retina-large.jpg"
+          },
+          {
+              "id": 27,
+              "name": "水煮牛肉Poached Sliced Beef in Hot Chili Oil",
+              "description": "Very spicy.",
+              "price": 17.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/89ad8679-346e-41d8-b98f-3501fff4b277-retina-large.jpg"
+          },
+          {
+              "id": 28,
+              "name": "Fried Rice炒饭",
+              "description": "With chopped broccoli, peas, carrots, bok choy, egg.",
+              "price": 9.5,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/ec06c431-9426-4971-a129-920440e1c9ce-retina-large.jpg"
+          },
+          {
+              "id": 29,
+              "name": "擂辣椒茄子皮蛋Smashed Green Pepper, Chinese Eggplant & Preserved Egg",
+              "description": "Very spicy.",
+              "price": 11.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/2fe1b87f-d41f-4fa4-8cae-5f2ee5bb97e4-retina-large.jpg"
+          },
+          {
+              "id": 30,
+              "name": "蒜蓉油麦菜Stir Fried A-Choy with Minced Garlic",
+              "description": "",
+              "price": 10.99,
+              "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/a307e73d-dd12-4841-be14-6f5825a64c59-retina-large.jpg"
+          }
+      ]
+  }
+]
 
-  return fetch(loginUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  }).then((response) => {
-    if (response.status < 200 || response.status >= 300) {
-      throw Error("Fail to log in");
-    }
-  });
+
+
+
+export const login = (credential) => {
+  return new Promise((resolve) => {
+    resolve();
+  })
+  // const loginUrl = `/login?username=${credential.username}&password=${credential.password}`;
+
+  // return fetch(loginUrl, {
+    // method: 'POST',
+    // headers: {
+      // 'Content-Type': 'application/json',
+    // },
+    // credentials: 'include',
+  // }).then((response) => {
+    // if (response.status < 200 || response.status >= 300) {
+      // throw Error('Fail to log in');
+    // }
+  // });
 };
 
 export const signup = (data) => {
-  const signupUrl = "/signup";
+  const signupUrl = '/signup';
 
   return fetch(signupUrl, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   }).then((response) => {
     if (response.status < 200 || response.status >= 300) {
-      throw Error("Fail to sign up");
+      throw Error('Fail to sign up');
     }
   });
 };
 
-export const getMenus = (restId) => {
-  return fetch(`/restaurant/${restId}/menu`).then((response) => {
+export const getMenus = (restId) =>
+  fetch(`/restaurant/${restId}/menu`).then((response) => {
     if (response.status < 200 || response.status >= 300) {
-      throw Error("Fail to get menus");
+      throw Error('Fail to get menus');
     }
 
     return response.json();
   });
-};
 
 export const getRestaurants = () => {
-  return fetch("/restaurants").then((response) => {
+return new Promise((resolve) => {
+  resolve(resData);
+});
+  // fetch('/restaurants').then((response) => {
+    // if (response.status < 200 || response.status >= 300) {
+      // throw Error('Fail to get restaurants');
+    // }
+
+    // return response.json();
+  // });
+}
+
+export const getCart = () =>
+  fetch('/cart').then((response) => {
     if (response.status < 200 || response.status >= 300) {
-      throw Error("Fail to get restaurants");
+      throw Error('Fail to get shopping cart data');
     }
 
     return response.json();
   });
-};
 
-export const getCart = () => {
-  return fetch("/cart").then((response) => {
+export const checkout = () =>
+  fetch('/checkout').then((response) => {
     if (response.status < 200 || response.status >= 300) {
-      throw Error("Fail to get shopping cart data");
-    }
-
-    return response.json();
-  });
-};
-
-export const checkout = () => {
-  return fetch("/checkout").then((response) => {
-    if (response.status < 200 || response.status >= 300) {
-      throw Error("Fail to checkout");
+      throw Error('Fail to checkout');
     }
   });
-};
 
-export const addItemToCart = (itemId) => {
-  return fetch(`/order/${itemId}`, {
-    method: "POST",
+export const addItemToCart = (itemId) =>
+  fetch(`/order/${itemId}`, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    credentials: "include",
+    credentials: 'include',
   }).then((response) => {
     if (response.status < 200 || response.status >= 300) {
-      throw Error("Fail to add menu item to shopping cart");
+      throw Error('Fail to add menu item to shopping cart');
     }
   });
-};
